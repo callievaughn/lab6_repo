d3ad0e57f5567dcc1662e5fd9ad3e057a8ea9827
callievaughn <122556798+callievaughn@users.noreply.github.com>
2023-02-22 12:33:57 -0500

    update

--- src/components/DurationExercise/index.js
+++ src/components/DurationExercise/index.js
@@ -6,9 +6,6 @@ export default function DurationExercise() {
     return (
         <div>
         <p>Running</p>
-        <p >Timer: 00:00:00</p>
-        <button>Start/Pause</button>
-        <button>Reset</button>
         <p>Mileage: </p>
         <button>Track Mileage</button>
         <button>Stop Tracking</button>
