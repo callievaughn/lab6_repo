1a9df0089d6bde5ddd4c3d9020e56e3eeede16e4
callievaughn <122556798+callievaughn@users.noreply.github.com>
2023-02-23 13:31:53 -0500

    final update

--- src/components/MileageExercise/index.js
+++ src/components/MileageExercise/index.js
@@ -20,19 +20,22 @@ const MileageExercise = () => {
         setMiles(!miles)
         clearInterval(currentMiles)
     }, [miles])
+ 
 
     let mile = (Math.floor((timed / (1000*60)) % 60)).toString().padStart(2, "0")
     let pointMile = (Math.floor((timed / 1000) % 60)).toString().padStart(2, "0", "0")
 
     return (
         <div>
+        <p style={{fontWeight:"bold"}}>Biking</p>
         <p>Current Mileage: {mile}.{pointMile} miles </p>
+        <p style = {{fontSize: ".9em"}}>Your average mile is 8:30 minutes.</p>
         <button onClick={startPause}>{miles ? "Pause" : "Start"}</button>
         <button onClick={()=> {
             setTimed(0)
         }}>Reset</button>
         <p style={{fontSize:".6em"}}>
-            Incrementing .01 miles every 5 seconds based on users speed for an average of a 8:30 minute mile.</p>
+            Incrementing .01 miles every 5 seconds.</p>
         </div>
     )
 }
